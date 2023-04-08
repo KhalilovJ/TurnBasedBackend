a25209bc167a7c67a52ba993deff8e6dadc2288a
Khalilov157 <evilcrewstudios@gmail.com>
2023-04-08 21:31:05 +0400

    front deleted

--- src/main/java/az/evilcastle/turnbased/TurnbasedApplication.java
+++ src/main/java/az/evilcastle/turnbased/TurnbasedApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class TurnbasedApplication {
