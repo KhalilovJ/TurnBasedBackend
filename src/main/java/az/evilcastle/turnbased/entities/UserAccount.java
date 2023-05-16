d01195a92d8e731986c7d80773dafb1253de297d
malishov <alishov.murad831@gmail.com>
2023-05-16 19:12:58 +0400

    Chanchged: Implemented UserDetails in UserAccount entity

--- src/main/java/az/evilcastle/turnbased/entities/UserAccount.java
+++ src/main/java/az/evilcastle/turnbased/entities/UserAccount.java
@@ -1,8 +1,15 @@
 package az.evilcastle.turnbased.entities;
 
+import az.evilcastle.turnbased.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
@@ -10,7 +17,7 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class UserAccount {
+public class UserAccount implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
@@ -19,7 +26,35 @@ public class UserAccount {
     String email;
     String password;
 
-//    @ManyToOne
-//    @JoinColumn(name = "active_session_id")
-//    Session activeSession;
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    //    @ManyToOne
+    //    @JoinColumn(name = "active_session_id")
+    //    Session activeSession;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
